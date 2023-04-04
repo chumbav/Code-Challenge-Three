d6090f6af06c0f59ae1bf6b04bba910d2e901a34
Victor <victor.chumba@student.moringaschool.com>
2023-04-04 15:48:19 +0300

    Completed code challenge

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let tickets_list = document.querySelector('.tickets-list')
+/*let tickets_list = document.querySelector('.tickets-list')
 let details = document.querySelector('.ticket-details')
+
 fetch('http://localhost:4000/films')
         .then(function (response) {
             return response.json();
@@ -17,7 +18,7 @@ fetch('http://localhost:4000/films')
             ticket.innerHTML = `
                 <p>${film.title}</p>
             `
-            availableTickets = film.capacity - film.tickets_sold
+            let availableTickets = film.capacity - film.tickets_sold
             tickets_list.appendChild(ticket)
             ticket.onclick = ()=>{
                 details.innerHTML=`
@@ -27,6 +28,84 @@ fetch('http://localhost:4000/films')
                     <p>${availableTickets}</p>
                     <button>Buy ticket</button>
                 `
+                let btn = document.querySelector('button')
+                btn.onclick = ()=>{
+                    film.tickets_sold+=1
+                    availableTickets = film.capacity - film.tickets_sold
+                    details.innerHTML=`
+                    <img height="500" src="${film.poster}" alt="${film.title}">
+                    <p>${film.description}</p>
+                    <p>${film.runtime}</p>
+                    <p>${availableTickets}</p>
+                    <button>Buy ticket</button>
+                `
+                let soldTickets = film.tickets_sold + 1
+                let final = {tickets_sold : soldTickets}
+                fetch(`http://localhost:4000/films/${film.id}`, {
+                        method: "PATCH",
+                        headers: {
+                            'Context-Type' : 'application/json'
+                        },
+                        body: JSON.stringify(final)
+                    })
+                    .then(res=>res.json())
+                    .then((data)=>console.log(data))
+                }
+            }
+        }
+    }*/
+    let tickets_list = document.querySelector('.tickets-list')
+let details = document.querySelector('.ticket-details')
+
+fetch('http://localhost:4000/films')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        displayTickets(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
+
+function displayTickets(films) {
+    for(let film of films){
+        let ticket = document.createElement('li')
+        ticket.innerHTML = `
+            <p>${film.title}</p>
+        `
+        let availableTickets = film.capacity - film.tickets_sold
+        tickets_list.appendChild(ticket)
+        ticket.onclick = ()=>{
+            details.innerHTML=`
+                <img height="500" src="${film.poster}" alt="${film.title}">
+                <p>${film.description}</p>
+                <p>${film.runtime}</p>
+                <p>${availableTickets}</p>
+                <button>Buy ticket</button>
+            `
+            let btn = document.querySelector('button')
+            btn.onclick = ()=>{
+                film.tickets_sold += 1
+                availableTickets = film.capacity - film.tickets_sold
+                details.innerHTML=`
+                    <img height="500" src="${film.poster}" alt="${film.title}">
+                    <p>${film.description}</p>
+                    <p>${film.runtime}</p>
+                    <p>${availableTickets}</p>
+                    <button>Buy ticket</button>
+                `
+                let final = {tickets_sold : film.tickets_sold}
+                fetch(`http://localhost:4000/films/${film.id}`, {
+                    method: "PATCH",
+                    headers: {
+                        'Content-Type' : 'application/json'
+                    },
+                    body: JSON.stringify(final)
+                })
+                .then(res=>res.json())
+                .then((data)=>console.log(data))
             }
         }
     }
+}
